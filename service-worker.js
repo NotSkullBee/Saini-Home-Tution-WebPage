0ed95082500f5f36f3c67363eab68011fc3431d8
Aman <notskullbee@gmail.com>
2026-03-19 18:16:56 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "saini-tuition-v1";
+
+/* FILES TO CACHE */
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/teacher.jpg",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+/* INSTALL */
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+/* ACTIVATE */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+/* FETCH */
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
